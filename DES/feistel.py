e35c1b08a4631068b7373dbb177ad4089e5fa1d1
Junhyuck-Woo <wjh0470@cau.ac.kr>
2020-05-04 23:23:17 +0900

    Finished swapper, mixer, feistel - des_func remain

--- DES/feistel.py
+++ DES/feistel.py
@@ -1,19 +1,29 @@
+from calculator import *
+
 class Feistel(object):
 
     def __init__(self):
+        cal = Calculator()
         pass
 
-    def __mixer(self, p, k):
-        return mid_code
+    def __mixer(self, l, r, k):
+        right = r
+        f_ = self.__des_function(r, k)
+        left = cal.xor(l, f_)
+        return left, right
 
-    def __swapper(self, p):
-        mid_code = p[31:] + p[:32]
+    def __swapper(self, l, r):
+        mid_code = r + l
         return mid_code
 
     def __des_function(self, p, k):
-        return mid_code
+
+        return 
 
     def run(self, input_string, key):
-        mid_code = self.__mixer(input_string, key)
-        mid_code = self.__swapper(mid_code)
+        left_text = input_string[:32]
+        right_text = input_string[31:]
+
+        left_text, right_text = self.__mixer(left_text, right_text, key)
+        mid_code = self.__swapper(left_text, right_text)
         return mid_code
