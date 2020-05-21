a9de7a129737f7c03225dfa02a11ee1b07a8b42c
Junhyuck-Woo <wjh0470@cau.ac.kr>
2020-05-22 04:24:19 +0900

    Upload new function

--- Functions/RelativePrimeFinder.py
+++ Functions/RelativePrimeFinder.py
@@ -1,15 +1,31 @@
-a = 1
-b = 33
-a_list = []
+import sys
 
-for a in range(b):
+class RelativePrimeFinder (object):
+    
+    n = 0
     count = 0
-    for i in range(2, b):
-        if (a%i==0) and (b%i==0):
-            count = count + 1
-            break
-    if count==0:
-        a_list.append(a)
+    a_list = []
+
+    def __init__(self, n):
+        self.n = int(n)
+
+    def find(self):
+        for a in range(self.n):
+            self.count=0
+            for i in range(2, self.n):
+                if (a%i==0) and (self.n%i==0):
+                    self.count = self.count + 1
+                    break
+            if self.count==0:
+                self.a_list.append(a)
+        print(self.a_list)
 
 
-print(a_list)
+if __name__ == "__main__":
+    # Check the input commend
+    if len(sys.argv) != 2:
+        print("Input error")
+        exit()
+
+    rpf = RelativePrimeFinder(sys.argv[1])
+    rpf.find()
